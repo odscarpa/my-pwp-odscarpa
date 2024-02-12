b4b805f027b990be316e3ec27e909af25eebce48
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-02-12 16:17:54 -0700

    redid navbar, created footer with links, added background image to about page and completed about page. added multiple images, added custom font, put priorities in home page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
     content: [
         "./index.html",
+        "./about.html",
+        "./issues.html",
         "./script.js"
 
     ],
@@ -11,7 +13,7 @@ module.exports = {
 
     // daisyUI config (optional - here are the default values)
     daisyui: {
-        themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+        themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
         darkTheme: "dark", // name of one of the included themes for dark mode
         base: true, // applies background color and foreground color for root element by default
         styled: true, // include daisyUI colors and design decisions for all components
@@ -21,6 +23,10 @@ module.exports = {
         themeRoot: ":root", // The element that receives theme color CSS variables
     },
     theme: {
-        extend: {},
-    },
+        extend: {
+                fontFamily: {
+                    'custom': ['Abril Fatface', 'serif']
+                }
+            },
+        },
 }
